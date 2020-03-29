28a83298dcd5a0b2a297f8f877df26888a7c0790
Unewe <Unewe@mail.ru>
2020-03-29 12:11:46 +0300

    liquibase

--- backend/src/main/java/com/unewej/questengine/model/User.java
+++ backend/src/main/java/com/unewej/questengine/model/User.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.NaturalId;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,17 +24,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    @Size(max = 40)
+    @NotEmpty
+    @Size(max = 50)
     private String name;
 
-    @NotBlank
-    @Size(max = 15)
+    @NotEmpty
+    @Size(max = 20)
     private String username;
 
     @NaturalId
-    @NotBlank
-    @Size(max = 40)
+    @NotEmpty
+    @Size(max = 50)
     @Email
     private String email;
 
--- readme.txt
+++ readme.txt
@@ -1,4 +1,5 @@
 Шаблон для создания нового сайта.
+Создать базу данных: Добавить название в application.properties и в liquibase.properties
 mvn clean install
 npm install
 npm start
